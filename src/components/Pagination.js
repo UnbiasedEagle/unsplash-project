d31e2142a5f8f285402c8343459027c7eb84b562
UnbiasedEagle <imsaurabhsingh007@gmail.com>
2020-09-13 08:31:44 +0530

    deploy

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -13,9 +13,9 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
       <ul className='pagination'>
         {pageNumbers.map(number => (
           <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='#' className='page-link'>
+            <span onClick={() => paginate(number)}  className='page-link'>
               {number}
-            </a>
+            </span>
           </li>
         ))}
       </ul>
